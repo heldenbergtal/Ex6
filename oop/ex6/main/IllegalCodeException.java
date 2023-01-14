0cfb4ea268dd0f3f38c90c82ee8a9be6055d39b7
talheldenberg <tal.heldenerg@mail.huji.ac.il>
2023-01-14 13:25:31 +0200

    illegalException

--- oop/ex6/main/IllegalCodeException.java
+++ oop/ex6/main/IllegalCodeException.java
@@ -1,2 +1,7 @@
-package oop.ex6.main;public class IllegalCodeException {
+package oop.ex6.main;
+
+public class IllegalCodeException extends Exception{
+    public IllegalCodeException(String errorMsg){
+        super(errorMsg);
+    }
 }
